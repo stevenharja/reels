440095a1b958998663c40b49cad86957c33aa6c6
stevenharja <stevenharja@gmail.com>
2020-10-27 21:06:16 +1100

    modified template, added footer, finished jumbotron, working on accordion, currently stable, replaced design from scss to use react's styled-components, and called normalize.css to help prevent unnecessary default styling

---
+++ src/global-styles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+html, body{
+    font-family: "Raleway", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+    "Helvetica Neue", sans-serif;
+    box-sizing: border-box;
+    font-size: 16px;
+    background-color: #000000;
+    color: #333333;
+}`;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
+// import "./index.scss";
+import "normalize.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { GlobalStyles } from "./global-styles";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
